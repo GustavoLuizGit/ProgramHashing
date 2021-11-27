948baecc828e654aba6d52433273b24e53885515
GustavoLuizGit <glogustavo4321@gmail.com>
2021-11-27 14:12:54 -0300

    Criação da tela principal e sua inicialização

---
+++ projeto-hash-com-deslocamento-main/projeto-hashing-com-deslocamento/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_hashing_com_deslocamento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MetodoHashing mh = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAdicionar_Click(object sender, EventArgs e)
+        {
+
+            mh = new MetodoHashing(txtChave.Text);
+            mh.MetodoHash();
+            switch (mh.testeretun())
+            {
+                case 0:
+                    txtResto0.Text += mh.Mostra();
+                    break;
+                case 1:
+                    txtResto1.Text += mh.Mostra();
+                    break;
+                case 2:
+                    txtResto2.Text += mh.Mostra();
+                    break;
+                case 3:
+                    txtResto3.Text += mh.Mostra();
+                    break;
+                case 4:
+                    txtResto4.Text += mh.Mostra();
+                    break;
+                case 5:
+                    txtResto5.Text += mh.Mostra();
+                    break;
+                case 6:
+                    txtResto6.Text += mh.Mostra();
+                    break;
+            }
+        }
+    }
+}
