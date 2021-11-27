6b504e61b3d555b2aea0735978f9de0406e31b31
GustavoLuizGit <glogustavo4321@gmail.com>
2021-11-27 14:12:30 -0300

    Partes necessarias do codigo

---
+++ projeto-hash-com-deslocamento-main/projeto-hashing-com-deslocamento/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace projeto_hashing_com_deslocamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAdicionar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtChave = new System.Windows.Forms.TextBox();
+            this.txtResto1 = new System.Windows.Forms.Label();
+            this.txtResto0 = new System.Windows.Forms.Label();
+            this.txtResto3 = new System.Windows.Forms.Label();
+            this.txtResto2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtResto5 = new System.Windows.Forms.Label();
+            this.txtResto4 = new System.Windows.Forms.Label();
+            this.txtResto6 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdAdicionar
+            // 
+            this.cmdAdicionar.FlatAppearance.BorderSize = 3;
+            this.cmdAdicionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAdicionar.ForeColor = System.Drawing.Color.White;
+            this.cmdAdicionar.Location = new System.Drawing.Point(291, 64);
+            this.cmdAdicionar.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdAdicionar.Name = "cmdAdicionar";
+            this.cmdAdicionar.Size = new System.Drawing.Size(139, 49);
+            this.cmdAdicionar.TabIndex = 0;
+            this.cmdAdicionar.Text = "Adicionar";
+            this.cmdAdicionar.UseVisualStyleBackColor = true;
+            this.cmdAdicionar.Click += new System.EventHandler(this.cmdAdicionar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 181);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resto 0:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(35, 214);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resto 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(35, 245);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resto 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(35, 334);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resto 5:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(35, 304);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Resto 4:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(35, 274);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 24);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Resto 3:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(35, 363);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Resto 6:";
+            // 
+            // txtChave
+            // 
+            this.txtChave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChave.ForeColor = System.Drawing.Color.Teal;
+            this.txtChave.Location = new System.Drawing.Point(39, 71);
+            this.txtChave.Margin = new System.Windows.Forms.Padding(4);
+            this.txtChave.Name = "txtChave";
+            this.txtChave.Size = new System.Drawing.Size(207, 34);
+            this.txtChave.TabIndex = 8;
+            // 
+            // txtResto1
+            // 
+            this.txtResto1.AutoSize = true;
+            this.txtResto1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto1.ForeColor = System.Drawing.Color.White;
+            this.txtResto1.Location = new System.Drawing.Point(131, 218);
+            this.txtResto1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto1.Name = "txtResto1";
+            this.txtResto1.Size = new System.Drawing.Size(0, 24);
+            this.txtResto1.TabIndex = 10;
+            // 
+            // txtResto0
+            // 
+            this.txtResto0.AutoSize = true;
+            this.txtResto0.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto0.ForeColor = System.Drawing.Color.White;
+            this.txtResto0.Location = new System.Drawing.Point(131, 185);
+            this.txtResto0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto0.Name = "txtResto0";
+            this.txtResto0.Size = new System.Drawing.Size(0, 24);
+            this.txtResto0.TabIndex = 9;
+            // 
+            // txtResto3
+            // 
+            this.txtResto3.AutoSize = true;
+            this.txtResto3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto3.ForeColor = System.Drawing.Color.White;
+            this.txtResto3.Location = new System.Drawing.Point(131, 283);
+            this.txtResto3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto3.Name = "txtResto3";
+            this.txtResto3.Size = new System.Drawing.Size(0, 24);
+            this.txtResto3.TabIndex = 17;
+            // 
+            // txtResto2
+            // 
+            this.txtResto2.AutoSize = true;
+            this.txtResto2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto2.ForeColor = System.Drawing.Color.White;
+            this.txtResto2.Location = new System.Drawing.Point(131, 250);
+            this.txtResto2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto2.Name = "txtResto2";
+            this.txtResto2.Size = new System.Drawing.Size(0, 24);
+            this.txtResto2.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(241, 283);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 24);
+            this.label8.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(241, 250);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 24);
+            this.label9.TabIndex = 18;
+            // 
+            // txtResto5
+            // 
+            this.txtResto5.AutoSize = true;
+            this.txtResto5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto5.ForeColor = System.Drawing.Color.White;
+            this.txtResto5.Location = new System.Drawing.Point(131, 334);
+            this.txtResto5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto5.Name = "txtResto5";
+            this.txtResto5.Size = new System.Drawing.Size(0, 24);
+            this.txtResto5.TabIndex = 21;
+            // 
+            // txtResto4
+            // 
+            this.txtResto4.AutoSize = true;
+            this.txtResto4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto4.ForeColor = System.Drawing.Color.White;
+            this.txtResto4.Location = new System.Drawing.Point(131, 304);
+            this.txtResto4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto4.Name = "txtResto4";
+            this.txtResto4.Size = new System.Drawing.Size(0, 24);
+            this.txtResto4.TabIndex = 20;
+            // 
+            // txtResto6
+            // 
+            this.txtResto6.AutoSize = true;
+            this.txtResto6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResto6.ForeColor = System.Drawing.Color.White;
+            this.txtResto6.Location = new System.Drawing.Point(131, 363);
+            this.txtResto6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtResto6.Name = "txtResto6";
+            this.txtResto6.Size = new System.Drawing.Size(0, 24);
+            this.txtResto6.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(33, 48);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 25);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Chave";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(484, 554);
+            this.Controls.Add(this.txtResto6);
+            this.Controls.Add(this.txtResto5);
+            this.Controls.Add(this.txtResto4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtResto3);
+            this.Controls.Add(this.txtResto2);
+            this.Controls.Add(this.txtResto1);
+            this.Controls.Add(this.txtResto0);
+            this.Controls.Add(this.txtChave);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdAdicionar);
+            this.Controls.Add(this.label10);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdAdicionar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtChave;
+        private System.Windows.Forms.Label txtResto1;
+        private System.Windows.Forms.Label txtResto0;
+        private System.Windows.Forms.Label txtResto3;
+        private System.Windows.Forms.Label txtResto2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label txtResto5;
+        private System.Windows.Forms.Label txtResto4;
+        private System.Windows.Forms.Label txtResto6;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
